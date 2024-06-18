3986e3037f48e393a375f7d2b477d5d52a10f6bb
HeenriqueCDS <heenriquecds@gmail.com>
2024-06-18 16:34:19 +0000

    chore: move modules to lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EventsModule } from './events/events.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { EventsModule } from '../libs/core/src/events/events.module';
+import { PrismaModule } from '../libs/core/src/prisma/prisma.module';
 import { SpotsModule } from './spots/spots.module';
 
 @Module({
